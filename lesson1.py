6490c4b9ddb82ec78d8e790f3af729acc3c4577b
Adriel3456 <the.dead.spy.squad.is.taken@gmail.com>
2025-06-13 20:02:43 -0600

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,15 @@
+country_dict = {
+    "Usa": "washington dc",
+    "Canada": "Ottawa",
+    "Mexico": "Mexico city",
+    "Russia": "Moscow",
+    "China": "Beijing",
+    "Japan": "Tokyo",
+    "India": "New Delhi",
+    "Germany": "Berlin",
+    "Brazil": "Brasilia",
+    "france": "Paris",}
+
+print(country_dict)
+print(country_dict["Usa"])
+print(country_dict.keys())
